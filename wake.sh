a55f7ab5e460df3eb0cb34c37baaf3dbbc34b437
zeroidentidad <jafs-2050@hotmail.com>
2021-01-12 20:08:22 -0600

    cambios

--- wake.sh
+++ wake.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-# ping when your job starts
-curl https://zerverus-d418bf.us1.kinto.io/api/extraviados?p=1
+# when your job starts
+curl https://zerverus-d418bf.us1.kinto.io/api/estatus/test
+ping zerverus-d418bf.us1.kinto.io
 
 wake.sh
 
 # ping when your job is finished
+curl https://zerverus-d418bf.us1.kinto.io/api/estatus/test
 ping zerverus-d418bf.us1.kinto.io
 
 # scheduling guide https://crontab.guru/
